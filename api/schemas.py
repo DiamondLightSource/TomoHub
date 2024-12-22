6621b2115f15db2b5b9ab91605b817e88e4e774d
itsdiy0 <diyo11jen@gmail.com>
2024-12-22 05:13:57 +0000

    added pydantic model to the requests and some minor edits

---
+++ api/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field, RootModel
+from typing import Dict, Any, Optional, Union
+
+class ParameterInfo(BaseModel):
+    """Schema for parameter information"""
+    type: str = Field(..., description="Type annotation of the parameter")
+    value: Any = Field(..., description="Default or required value of the parameter")
+    desc: str = Field(..., description="Description of the parameter")
+
+class MethodTemplate(BaseModel):
+    """Schema for individual method template"""
+    method: str = Field(..., description="Name of the method")
+    module_path: str = Field(..., description="Full path to the module containing the method")
+    method_desc: str = Field(..., description="Description of what the method does")
+    parameters: Dict[str, ParameterInfo] = Field(..., description="Dictionary of parameter information")
+
+class ModuleTemplates(RootModel):
+    """Schema for all methods in a module"""
+    root: Dict[str, MethodTemplate] = Field(..., description="Dictionary of method templates in a module")
+
+class AllTemplates(RootModel):
+    """Schema for all modules and their methods"""
+    root: Dict[str, Dict[str, MethodTemplate]] = Field(..., description="Dictionary of all modules and their method templates")
