6192ae37fb501cc5dea80d2b2edf7ab25be75228
itsdiy0 <diyo11jen@gmail.com>
2025-01-20 09:08:01 +0000

    restructred directories

---
+++ backend/loaders.py
@@ -0,0 +1,10 @@
+basic_standard_loader = {
+    "method_name" : "standard_tomo",
+    "module_path" : "httomo.data.hdf.loaders",
+    "parameters" : {
+        "data_path":{
+            "type" : "str",
+            "value" : "/entry1/tomo_entry/data/data",
+        }
+    }
+}
