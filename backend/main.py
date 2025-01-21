23939cfc8f2b661b5c1270a1b9f4e261a3511591
itsdiy0 <diyo11jen@gmail.com>
2025-01-21 06:08:51 +0000

    added image denoising and artefact removal methods and some other minor modifications

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException
-import importlib
 from generator import generate_method_template
-from schemas import AllTemplates
+from Models.MethodsTemplate import AllTemplates
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict, List
+from methods import METHOD_CATEGORIES,standard_tomo_loader
 
 app = FastAPI()
 
@@ -12,6 +12,7 @@ origins = [
     "http://127.0.0.1:5173",
 ]
 
+#CORS 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -20,48 +21,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Define method categories
-METHOD_CATEGORIES = {
-    "image-saving": {
-        "httomolib.misc.images": ["save_to_images"],
-        "httomolibgpu.misc.rescale": ["rescale_to_int"]
-    },
-    "segmentation": {
-        "httomolib.misc.segm": ["binary_thresholding"]
-    },
-    "morphological": {
-        "httomolib.misc.morph": ["data_reducer"],
-        "httomolibgpu.misc.morph": ["sino_360_to_180", "data_resampler"]
-    },
-    "normalization": {
-        "httomolibgpu.prep.normalize": ["normalize"]
-    },
-    "phase-retrieval": {
-        "httomolib.prep.phase": ["paganin_filter"],
-        "httomolibgpu.prep.phase": ["paganin_filter_savu", "paganin_filter_tomopy"]
-    },
-    "stripe-removal": {
-        "httomolibgpu.prep.stripe": [
-            "remove_stripe_based_sorting",
-            "remove_stripe_ti",
-            "remove_all_stripe",
-            "raven_filter"
-        ]
-    },
-    "distortion-correction": {
-        "httomolibgpu.prep.alignment": ["distortion_correction_proj_discorpy"]
-    },
-    "rotation-center": {
-        "httomolibgpu.recon.rotation": [
-            "find_center_vo",
-            "find_center_360",
-            "find_center_pc"
-        ]
-    },
-    "reconstruction": {
-        "httomolibgpu.recon.algorithm": ["FBP", "LPRec", "SIRT", "CGLS"]
-    }
-}
 
 def get_methods_templates(module_methods: Dict[str, List[str]]) -> Dict:
     """
@@ -111,4 +70,4 @@ async def get_all_methods():
     
 @app.get("/methods/loaders", response_model=AllTemplates)
 async def get_loader_method():
-    pass
+    return standard_tomo_loader
