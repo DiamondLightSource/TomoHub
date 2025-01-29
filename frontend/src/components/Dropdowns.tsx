63f0da53b52fc98414779aef30200e232d6a97a4
itsdiy0 <diyo11jen@gmail.com>
2025-01-29 15:10:03 +0000

    modifed the guide section

--- frontend/src/components/Dropdowns.tsx
+++ frontend/src/components/Dropdowns.tsx
@@ -35,7 +35,7 @@ const Dropdowns: React.FC = () => {
       </Tabs>
 
       {/* Tab Content */}
-      <Box sx={{ p: 3, width: "900px" }}>
+      <Box sx={{ p: 3, width: "100%" }}>
         {value === 0 && (
           <Guide/>
         )}
