7a6b26c31f12e0208ea11e9ddbefc7ef40b155bb
itsdiy0 <diyo11jen@gmail.com>
2025-01-29 15:10:03 +0000

    modifed the guide section

--- frontend/src/components/Guide.tsx
+++ frontend/src/components/Guide.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import InfoIcon from "@mui/icons-material/Info";
 import SettingsIcon from '@mui/icons-material/Settings';
+import { Card, Typography } from "@mui/material";
 
 const Guide: React.FC = () => {
   return (
-    <div className="guide">
-      <h1>Guide</h1>
-      <p>
-
-      <li>Tomohub is a graphical tool to generate YAML process lists for running HTTOMO.</li>
-A process list is having a loader and a set of methods based on your need for the reconstruction job.
-You can start by first setting data for HTTOMO loader, then you can select the set of methods you required.
-After setting the loader, you can start selecting your methods, it’s suggested to select your methods in the order appeared from the vertical tab on the left (pre-processing, reconstruction, post-processing, image saving).
-Each method has a <InfoIcon/> icon, which will open the documentation of that method in a new tab for further details if required.
-You can view and edit your methods in the sidebar on right, you can also reorder your methods in the process list simply by drag and dropping them to the way you desire.
-Parameter sweeping : next to numeric field types (integers/floats) you can see <SettingsIcon/> icon, you can use this button to enable parameter sweeping feature for that parameter, with this feature you can set more than one value for a parameter, you can use it in 2 ways, use a range system (start,stop,step) or with constant values (separated with “,”) 
-HTTOMO currently supports only one parameter sweeping per process list.
-At the end, you can name your process list file and simply download it by clicking on “GET CONFIG FILE"
-      </p>
-    </div>
+       
+       <Card variant="outlined" sx={{ mx: "auto", mb: 2, p: 2, border: "1px solid #89987880", borderRadius: "4px" }}>
+        <Typography variant="h6">
+          What is tomohub ?
+        </Typography>
+        <Typography variant="body1" gutterBottom> 
+        Tomohub is a graphical tool to generate yaml processlists to run construction jobs with <a href="https://diamondlightsource.github.io/httomo/index.html" target="_blank">HTTOMO package</a>  
+        </Typography>
+        <Typography variant="h6">
+          How to start ?
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+        Start by setting up your loader and then select your required methods afterwards 
+        </Typography>
+        <Typography variant="h6">
+          Further information
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Please visit Tomohub's<a href="https://github.com/DiamondLightSource/TomoHub" target="_blank"> github</a> page 
+        </Typography>
+       </Card>
   );
 };
 
