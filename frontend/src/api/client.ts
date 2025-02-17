37318a1582249a2d67e2e26b7079300de2d1a6dd
itsdiy0 <diyo11jen@gmail.com>
2025-02-17 15:37:46 +0000

    changes for VITE env variable injection happening with nginx

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,12 +2,10 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "localhost:8000",
+  baseURL: window.__ENV__?.VITE_API_BASE_URL || "http://localhost:8000",
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-
 
 export default apiClient;
