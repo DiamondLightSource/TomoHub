e03fb457a34917b8a1cf3a123787c0d0525688a9
Mobin Rajaei <diyo11jen@gmail.com>
2025-08-18 04:14:51 +0100

    fix: changed tsconfig for workflows-lib CI error 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import relay from 'vite-plugin-relay'
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import relay from 'vite-plugin-relay';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ''); // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(),relay,tsconfigPaths()],
-    define: {
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL),
-      // You can add other environment variables here if needed, for example:
-      // 'import.meta.env.OTHER_ENV_VAR': JSON.stringify(env.OTHER_ENV_VAR),
+    plugins: [react(), relay, tsconfigPaths()],
+    resolve: {
+      alias: {
+        'workflows-lib': path.resolve(__dirname, 'node_modules/workflows-lib/dist')
+      }
     },
-  }
-})
+    define: {
+      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL)
+    }
+  };
+});
