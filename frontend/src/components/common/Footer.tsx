30171d724cdb64913a0676b3289141468e1ffab5
itsdiy0 <diyo11jen@gmail.com>
2025-08-25 23:05:37 +0100

    test husky 2

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -19,10 +19,8 @@ export default function Footer() {
           target="_blank"
           href="https://www.diamond.ac.uk/Home.html"
           underline="hover"
-          
-          
-          >
-                  Diamond Light Sources Ltd
+        >
+          Diamond Light Sources Ltd
         </Link>
       </Typography>
     </Box>
