7831cd7ac675f8abb93ce58c17501cdef831e8e8
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2025-09-02 13:27:58 +0100

    Remove unnecessary vite env var config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import relay from "vite-plugin-relay";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react(), relay],
-    define: {
-      "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
-        env.VITE_API_BASE_URL
-      ),
-    },
-  };
+export default defineConfig({
+  plugins: [react(), relay],
 });
