261d59380e768d1ee1058ef14ca1bc3f111c6228
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2025-09-09 09:30:55 +0100

    Remove tools integrating prettier and eslint
    For simplicitly, for now, it will be sufficient to use the two tools
    (prettier and eslint) independently, rather than having some kind of
    integration of them.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,8 +4,6 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -25,7 +23,6 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       react,
-      prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -35,7 +32,6 @@ export default tseslint.config(
       ],
       "react/react-in-jsx-scope": "off",
       "react/jsx-uses-react": "off",
-      "prettier/prettier": "error",
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_" },
@@ -46,6 +42,5 @@ export default tseslint.config(
         version: "detect",
       },
     },
-  },
-  prettierConfig
+  }
 );
