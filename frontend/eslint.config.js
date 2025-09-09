2c709a04c1f4c5cf77ab718072beeb67f32b6d07
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2025-09-09 13:13:45 +0100

    Add eslint rule for pattern to ignore unused var

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -32,7 +32,7 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": [
         "error",
-        { argsIgnorePattern: "^_" },
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
     },
     settings: {
