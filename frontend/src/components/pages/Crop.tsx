f0a8e57007bf954f185606080d5b96db84c6b941
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-10 14:42:49 +0000

    Cropping page and respective button added

---
+++ frontend/src/components/pages/Crop.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import { Box } from "@mui/material";
+// import Guide from "../common/Guide";
+
+const Crop: React.FC = () => {
+  return <h1>Cropping page</h1>;
+};
+
+export default Crop;
