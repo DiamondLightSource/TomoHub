c9d1f4de52624fa940171b16bede4376b44a61c9
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-22 14:42:24 +0000

    Big data added to plot

--- frontend/src/components/pages/Crop.tsx
+++ frontend/src/components/pages/Crop.tsx
@@ -4,25 +4,30 @@ import React from "react";
 
 import { HeatmapPlot, ScaleType } from "@diamondlightsource/davidia";
 import test_data from "./crop_test_data/test-projection.json";
+import big_test_data from "./crop_test_data/real-projection.json";
 import type { NDT } from "@diamondlightsource/davidia";
 import ndarray from "ndarray";
 
 const Crop: React.FC = () => {
-  const test_data_typed: Uint16Array = new Uint16Array(test_data);
-  const test_data_NDT = ndarray(test_data_typed, [128, 160]) as NDT;
+  const big_test_data_typed: number[] = big_test_data;
+  const big_test_data_array: Uint16Array = new Uint16Array(big_test_data_typed);
+  const big_test_data_NDT = ndarray(big_test_data_array, [2160, 2560]) as NDT;
+
+  const test_data_array: Uint16Array = new Uint16Array(test_data);
+  const test_data_NDT = ndarray(test_data_array, [128, 160]) as NDT;
 
   const heatMap = (
     <HeatmapPlot
       aspect="auto"
       colourMap="Purples"
-      domain={[0, 110]}
+      domain={[0, 60000]}
       heatmapScale={ScaleType.Linear}
       plotConfig={{
         title: "Sample Heatmap Plot",
         xLabel: "x-axis",
         yLabel: "y-axis",
       }}
-      values={test_data_NDT}
+      values={big_test_data_NDT}
       selectionsListener={(e, f, g) => {
         console.log(g);
       }}
