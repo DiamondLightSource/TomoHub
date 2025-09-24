0d80a162b1a0bfd7d1da2931979c66fea445b86c
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-24 09:08:52 +0000

    magic numbers removed, better variable names used

--- frontend/src/components/pages/Crop.tsx
+++ frontend/src/components/pages/Crop.tsx
@@ -1,28 +1,31 @@
 import React from "react";
 
 import { HeatmapPlot, ScaleType } from "@diamondlightsource/davidia";
-import big_test_data from "./crop_test_data/real-projection.json";
+import raw_image from "./crop_test_data/real-projection.json";
 import type { NDT } from "@diamondlightsource/davidia";
 import ndarray from "ndarray";
 
 const Crop: React.FC = () => {
-  const big_test_data_array: Uint16Array = new Uint16Array(
-    big_test_data as number[]
-  );
-  const big_test_data_NDT = ndarray(big_test_data_array, [2160, 2560]) as NDT;
+  const image_width = 2560;
+  const image_height = 2160;
+  const max_pixel_value = 60000;
+
+  const image_array: Uint16Array = new Uint16Array(raw_image as number[]);
+
+  const image_NDT = ndarray(image_array, [image_height, image_width]) as NDT;
 
   const heatMap = (
     <HeatmapPlot
       aspect="auto"
       colourMap="Purples"
-      domain={[0, 60000]}
+      domain={[0, max_pixel_value]}
       heatmapScale={ScaleType.Linear}
       plotConfig={{
         title: "Sample Heatmap Plot",
         xLabel: "x-axis",
         yLabel: "y-axis",
       }}
-      values={big_test_data_NDT}
+      values={image_NDT}
       selectionsListener={(e, f, g) => {
         console.log(g);
       }}
