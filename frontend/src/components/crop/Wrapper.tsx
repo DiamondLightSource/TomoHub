85b46503522bfb7bdfb315c849d3d1689dc4ac2c
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-26 10:02:51 +0000

    Created a file for selection SelectionOperations Moved state to wrapper

--- frontend/src/components/crop/Wrapper.tsx
+++ frontend/src/components/crop/Wrapper.tsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 import ImagePlot from "../crop/Plot";
 import ImageNavbar from "../crop/Navbar";
-import type { NDT } from "@diamondlightsource/davidia";
+import type { NDT, SelectionBase } from "@diamondlightsource/davidia";
+import defineSelectionOperations from "./SelectionOperations";
+import type { selectionOperations } from "./SelectionOperations";
 
 interface WrapperProps {
   max_pixel_value: number;
@@ -16,14 +18,44 @@ export default function DisplayAreaWrapper({
   images,
 }: WrapperProps) {
   const [imageIndex, setImageIndex] = useState(0);
+  // I couldnt figure out how to make an empty array of fixed length for a custom type like SelectionBase
+  // even with ndarray
+  // this will also run every refresh but it doesnt make sense to create it in a parent component and pass it down
+  const emptyArray: SelectionBase[][] = [];
+  for (let i = 0; i < copies; i++) {
+    emptyArray.push([]);
+  }
+  const [imageSelections, setSelections] = useState(emptyArray);
+
+  // the selection currently being presented on the screen
+  let on_screen_selections: SelectionBase[] = [];
+  let on_screen_selection_index = -1;
+  // setting currentSelections
+  // searches backwards from the current frame for the first previous selection that is not empty
+  // + copies and modulo create looping effect
+  for (let i = imageIndex + copies; i > 0; i--) {
+    const iteration_selection = imageSelections[i % copies];
+    if (iteration_selection.length != 0) {
+      on_screen_selections = iteration_selection;
+      on_screen_selection_index = i % copies;
+      break;
+    }
+  }
+
+  const selection_operations: selectionOperations = defineSelectionOperations(
+    imageIndex,
+    on_screen_selection_index,
+    imageSelections,
+    setSelections
+  );
 
   return (
     <div>
       <ImagePlot
         image={images[imageIndex]}
         max_pixel_value={max_pixel_value}
-        index={imageIndex}
-        copies={copies}
+        on_screen_selections={on_screen_selections}
+        selection_operations={selection_operations}
       />
       <ImageNavbar
         totalImages={copies}
