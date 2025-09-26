2ffd1601d8410bc8ece45bdc9f81a9aaacc8e11f
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-26 10:21:58 +0000

    Functionality added to remove selection button

--- frontend/src/components/crop/Wrapper.tsx
+++ frontend/src/components/crop/Wrapper.tsx
@@ -62,6 +62,7 @@ export default function DisplayAreaWrapper({
         totalImages={copies}
         currentImageIndex={imageIndex}
         imageIndexSetter={setImageIndex}
+        selection_operations={selection_operations}
       />
     </div>
   );
