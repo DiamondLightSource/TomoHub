2bca2e66a6f93d2b643f3fecb0b0365406e40c14
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-26 10:02:51 +0000

    Created a file for selection SelectionOperations Moved state to wrapper

--- frontend/src/components/crop/Plot.tsx
+++ frontend/src/components/crop/Plot.tsx
@@ -5,152 +5,21 @@ import {
   SelectionBase,
   RectangularSelection,
 } from "@diamondlightsource/davidia";
-import { useMemo, useState } from "react";
+import type { selectionOperations } from "./SelectionOperations";
 
 interface ImagePlotProps {
   image: NDT;
-  index: number;
+  on_screen_selections: SelectionBase[];
   max_pixel_value: number;
-  copies: number;
-}
-
-// creates selection at the current index
-function createSelection(
-  index: number,
-  imageSelectionsCopy: SelectionBase[][],
-  setSelections: React.Dispatch<React.SetStateAction<SelectionBase[][]>>,
-  selection: SelectionBase
-) {
-  imageSelectionsCopy[index] = [selection];
-  setSelections(imageSelectionsCopy);
-}
-
-// removes on screen selection
-function removeSelection(
-  onScreenSelectionIndex: number,
-  imageSelectionsCopy: SelectionBase[][],
-  setSelections: React.Dispatch<React.SetStateAction<SelectionBase[][]>>
-) {
-  imageSelectionsCopy[onScreenSelectionIndex] = [];
-  setSelections(imageSelectionsCopy);
-}
-
-function onScreenBeingModified(
-  onScreenSelectionIndex: number,
-  imageSelectionsCopy: SelectionBase[][],
-  selection: SelectionBase
-): boolean {
-  return imageSelectionsCopy[onScreenSelectionIndex][0] == selection;
-}
-
-function forceRefresh(
-  index: number,
-  onScreenSelectionIndex: number,
-  imageSelectionsCopy: SelectionBase[][],
-  setSelections: React.Dispatch<React.SetStateAction<SelectionBase[][]>>
-) {
-  // copy the value of currentSelection and set it to that again (dont change it)
-  // this stops regions being added if theyre not a rectangle
-  // however, the component still needs to refresh as the new selection region will be visible otherwise
-  // lmk if theres a better way to "force refresh" a component
-  if (onScreenSelectionIndex == -1) {
-    onScreenSelectionIndex = index;
-  }
-  const currentSelectionsCopy = [
-    ...imageSelectionsCopy[onScreenSelectionIndex],
-  ];
-  imageSelectionsCopy[onScreenSelectionIndex] = currentSelectionsCopy;
-  setSelections(imageSelectionsCopy);
-}
-
-type selectionOperations = {
-  minCreateSelection: (selection: SelectionBase) => void;
-  minRemoveSelection: () => void;
-  minOnScreenBeingModified: (selection: SelectionBase) => boolean;
-  minForceRefresh: () => void;
-};
-
-function defineSelectionOperations(
-  index: number,
-  onScreenSelectionIndex: number,
-  imageSelections: SelectionBase[][],
-  setSelections: React.Dispatch<React.SetStateAction<SelectionBase[][]>>
-): selectionOperations {
-  const imageSelectionsCopy = [...imageSelections];
-  const function_holder: selectionOperations = {
-    minCreateSelection: function (selection: SelectionBase) {
-      createSelection(index, imageSelectionsCopy, setSelections, selection);
-    },
-    minRemoveSelection: function () {
-      removeSelection(
-        onScreenSelectionIndex,
-        imageSelectionsCopy,
-        setSelections
-      );
-    },
-    minOnScreenBeingModified: function (selection: SelectionBase): boolean {
-      return onScreenBeingModified(
-        onScreenSelectionIndex,
-        imageSelectionsCopy,
-        selection
-      );
-    },
-    minForceRefresh: function () {
-      forceRefresh(
-        index,
-        onScreenSelectionIndex,
-        imageSelectionsCopy,
-        setSelections
-      );
-    },
-  };
-  return function_holder;
+  selection_operations: selectionOperations;
 }
 
 export default function ImagePlot({
   image,
-  index,
+  on_screen_selections,
   max_pixel_value,
-  copies,
+  selection_operations,
 }: ImagePlotProps) {
-  // useMemo so the empty array is only created once (unless copies is updated)
-  const emptyArray: SelectionBase[][] = useMemo(() => {
-    const result: SelectionBase[][] = [];
-    for (let i = 0; i < copies; i++) {
-      result.push([]);
-    }
-    return result;
-  }, [copies]);
-
-  const [imageSelections, setSelections] = useState(emptyArray);
-
-  // the selection currently being presented on the screen
-  let onScreenSelections: SelectionBase[] = [];
-  let onScreenSelectionIndex = -1;
-  // setting currentSelections
-  // searches backwards from the current frame for the first previous selection that is not empty
-  // + copies and modulo create looping effect
-  for (let i = index + copies; i > 0; i--) {
-    const iteration_selection = imageSelections[i % copies];
-    if (iteration_selection.length !== 0) {
-      onScreenSelections = iteration_selection;
-      onScreenSelectionIndex = i % copies;
-      break;
-    }
-  }
-
-  const {
-    minCreateSelection,
-    minRemoveSelection,
-    minOnScreenBeingModified,
-    minForceRefresh,
-  } = defineSelectionOperations(
-    index,
-    onScreenSelectionIndex,
-    imageSelections,
-    setSelections
-  );
-
   return (
     <Box style={{ display: "grid", height: "49vh", minHeight: "400px" }}>
       <HeatmapPlot
@@ -170,22 +39,22 @@ export default function ImagePlot({
           }
             if (eventType === "created") {
               if (selection instanceof RectangularSelection) {
-                minCreateSelection(selection);
+                selection_operations.minCreateSelection(selection);
               } else {
                 // selection area is not a rectangle
                 // dont add anything to the list and force refresh so it disapears
-                minForceRefresh();
+                selection_operations.minForceRefresh();
               }
             } else if (eventType === "removed") {
-              minRemoveSelection();
+              selection_operations.minRemoveSelection();
             } else if (eventType === "updated" && !dragging) {
-              if (!minOnScreenBeingModified(selection)) {
-                minCreateSelection(selection);
+              if (!selection_operations.minOnScreenBeingModified(selection)) {
+                selection_operations.minCreateSelection(selection);
               }
             }
           }
         }
-        selections={onScreenSelections}
+        selections={on_screen_selections}
       />
     </Box>
   );
--- frontend/src/components/crop/Wrapper.tsx
+++ frontend/src/components/crop/Wrapper.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 import ImagePlot from "../crop/Plot";
 import ImageNavbar from "../crop/Navbar";
-import type { NDT } from "@diamondlightsource/davidia";
+import type { NDT, SelectionBase } from "@diamondlightsource/davidia";
+import defineSelectionOperations from "./SelectionOperations";
+import type { selectionOperations } from "./SelectionOperations";
 
 interface WrapperProps {
   max_pixel_value: number;
@@ -16,14 +18,45 @@ export default function DisplayAreaWrapper({
   images,
 }: WrapperProps) {
   const [imageIndex, setImageIndex] = useState(0);
+  // useMemo so the empty array is only created once (unless copies is updated)
+  const emptyArray: SelectionBase[][] = useMemo(() => {
+    const result: SelectionBase[][] = [];
+    for (let i = 0; i < copies; i++) {
+      result.push([]);
+    }
+    return result;
+  }, [copies]);
+  const [imageSelections, setSelections] = useState(emptyArray);
+
+  // the selection currently being presented on the screen
+  let on_screen_selections: SelectionBase[] = [];
+  let on_screen_selection_index = -1;
+  // setting currentSelections
+  // searches backwards from the current frame for the first previous selection that is not empty
+  // + copies and modulo create looping effect
+  for (let i = imageIndex + copies; i > 0; i--) {
+    const iteration_selection = imageSelections[i % copies];
+    if (iteration_selection.length != 0) {
+      on_screen_selections = iteration_selection;
+      on_screen_selection_index = i % copies;
+      break;
+    }
+  }
+
+  const selection_operations: selectionOperations = defineSelectionOperations(
+    imageIndex,
+    on_screen_selection_index,
+    imageSelections,
+    setSelections
+  );
 
   return (
     <div>
       <ImagePlot
         image={images[imageIndex]}
         max_pixel_value={max_pixel_value}
-        index={imageIndex}
-        copies={copies}
+        on_screen_selections={on_screen_selections}
+        selection_operations={selection_operations}
       />
       <ImageNavbar
         totalImages={copies}
