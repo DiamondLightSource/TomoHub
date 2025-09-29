ea6d303928c134da2a8b365308654e44b3358d83
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-29 14:43:05 +0000

    Preview values are set using the Contextbar

--- frontend/src/components/crop/Contextbar.tsx
+++ frontend/src/components/crop/Contextbar.tsx
@@ -1,8 +1,11 @@
 import { Box, Button, Grid2 } from "@mui/material";
 import SaveOutlined from "@mui/icons-material/SaveOutlined";
 import FileOpenOutlined from "@mui/icons-material/FileOpenOutlined";
+import { useLoader, PreviewType } from "../../contexts/LoaderContext";
 
 export default function Contextbar() {
+  const { setDetectorX, setDetectorY } = useLoader();
+
   return (
     <Box style={{ height: "6vh", minHeight: "50px" }}>
       <Grid2 container spacing={2} sx={{ alignItems: "center" }}>
@@ -14,7 +17,16 @@ export default function Contextbar() {
         </Grid2>
         <Grid2 size={8} />
         <Grid2 size={1}>
-          <Button variant="outlined">
+          <Button
+            variant="outlined"
+            onClick={() => {
+              console.log("running!");
+              const xValues: PreviewType = { start: 10, stop: 90 };
+              setDetectorX(xValues);
+              const yValues: PreviewType = { start: 20, stop: 80 };
+              setDetectorY(yValues);
+            }}
+          >
             <SaveOutlined fontSize="large" />
           </Button>
         </Grid2>
