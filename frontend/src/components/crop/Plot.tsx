60768e80f62c7dbad17ae574dd15a17cfcbbc4d6
Oli Jones <oliver.jones@diamond.ac.uk>
2025-09-30 15:22:27 +0000

    Fixed bug where adding a new selection "unmodifies" existing selection

--- frontend/src/components/crop/Plot.tsx
+++ frontend/src/components/crop/Plot.tsx
@@ -28,16 +28,21 @@ export default function ImagePlot({ image, max_pixel_value }: ImagePlotProps) {
           yLabel: "y-axis",
         }}
         values={image}
-        selectionsListener={(eventType, _, selection) => {
-          if (eventType == "created" && selection != undefined) {
-            if (selection instanceof RectangularSelection) {
+        selectionsListener={(eventType, dragging, selection) => {
+          if (selection != undefined) {
+            if (eventType == "created") {
+              if (selection instanceof RectangularSelection) {
+                setCurrentSelection([selection]);
+              } else {
+                // copy the value of currentSelection and set it to that again (dont change it)
+                // this stops regions being added if theyre not a rectangle
+                // however, the component still needs to refresh as the new selection region will be visible otherwise
+                // lmk if theres a better way to "force refresh" a component
+                setCurrentSelection([...currentSelection]);
+              }
+            }
+            if (eventType == "updated" && !dragging) {
               setCurrentSelection([selection]);
-            } else {
-              // copy the value of currentSelection and set it to that again (dont change it)
-              // this stops regions being added if theyre not a rectangle
-              // however, the component still needs to refresh as the new selection region will be visible otherwise
-              // lmk if theres a better way to "force refresh" a component
-              setCurrentSelection([...currentSelection]);
             }
           }
         }}
