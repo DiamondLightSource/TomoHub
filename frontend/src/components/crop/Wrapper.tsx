692aec0ffbd5f449a7e538a0cef97ac6fe5736a9
Oli Jones <oliver.jones@diamond.ac.uk>
2025-10-14 12:22:03 +0000

    Simplified paths in Wrapper class

--- frontend/src/components/crop/Wrapper.tsx
+++ frontend/src/components/crop/Wrapper.tsx
@@ -1,11 +1,12 @@
 import React, { useMemo, useState } from "react";
 
 import Contextbar from "./Contextbar";
-import ImagePlot from "../crop/Plot";
-import ImageNavbar from "../crop/Navbar";
+import ImagePlot from "./Plot";
+import ImageNavbar from "./Navbar";
 import type { NDT, RectangularSelection } from "@diamondlightsource/davidia";
 import defineSelectionOperations from "./SelectionOperations";
 import type { SelectionOperations } from "./SelectionOperations";
+import { SelectionMode } from "/workspace/frontend/src/types/crop.ts";
 
 interface WrapperProps {
   maxPixelValue: number;
