e3a7d6fcd46bdc8dbaf3cb40d093bc9e1d2448db
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2026-03-02 16:53:54 +0000

    Restructure codebase into general imaging web app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,16 @@
-import React, { useState, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/pages/Home";
-import Layout from "./components/common/Layout";
-import Methods from "./components/pages/Methods";
-import FullPipelines from "./components/pages/FullPipelines";
-import Submission from "./components/workflows/Submission";
-import Crop from "./components/pages/Crop";
-import { Visit } from "@diamondlightsource/sci-react-ui";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { default as TomographyApp } from "../tomography/src/App";
+import Layout from "./Layout";
 
 const App: React.FC = () => {
-  const [userVisit, setUserVisit] = useState<Visit>();
   return (
-    <Router>
-      <Suspense fallback={<div>Loading workflow...</div>}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="methods" element={<Methods />} />
-            <Route path="fullpipelines" element={<FullPipelines />} />
-            <Route
-              path="workflow-run"
-              element={
-                <Submission
-                  workflowName="httomo-gpu-job"
-                  setVisit={setUserVisit}
-                />
-              }
-            />
-            <Route
-              path="workflow-cor"
-              element={
-                <Submission
-                  workflowName="httomo-cor-sweep"
-                  setVisit={setUserVisit}
-                />
-              }
-            />
-            <Route path="crop" element={<Crop setVisit={setUserVisit} />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="tomography/*" element={<TomographyApp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
